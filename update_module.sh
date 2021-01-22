dbf147c79919113acc049bb2ed8f4190a3c48c91
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-22 22:40:18 +0100

    initial commit

---
+++ update_module.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module_name='choria-mcollective_util_actionpolicy'
+module_version='3.2.0'
+
+echo 'Updating module'
+
+wget -c "https://forge.puppet.com/v3/files/${module_name}-${module_version}.tar.gz" -O - | tar -xz
+
+mv ${module_name}-${module_version}/* .
+
+rm -rfv "${module_name}-${module_version}"
